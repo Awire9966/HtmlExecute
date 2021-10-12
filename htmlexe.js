250fb00f81ad0f1674eb37a556b1c543be55ecc2
Awire9966 <65136929+Awire9966@users.noreply.github.com>
2021-10-12 12:07:31 -0400

    Update htmlexe.js

--- htmlexe.js
+++ htmlexe.js
@@ -1,1 +1,6 @@
 
+function runhtml() {
+    var myWindow1 = window.open('', 'Your Code', 'scrollbars=1,height='+screen.availHeight+',width='+screen.availWidth);
+   var codetextbox = document.getElementById("codebox")
+    myWindow1.document.write(codetextbox.value);
+}
